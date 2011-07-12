3a4a2f0141366e25d9439bc8d46221d2c040d990
kamekoopa <hogehugo.gmail.com>
2011-07-12 21:01:30 +0900

    traitとinterfaceを組み合わせて型システムに組み込むテスト

---
+++ trait_type.php
@@ -0,0 +1,65 @@
+<?php
+
+interface Runnable{
+  public function run();
+}
+
+trait RunByLeg{
+  public function run(){
+    echo "足で走る\n";
+  }
+}
+
+trait RunByWheel{
+  public function run(){
+    echo "車輪で走る\n";
+  }
+}
+
+trait RunByHorse{
+  public function run(){
+    echo "馬で走る\n";
+  }
+}
+
+
+class Human implements Runnable {
+  use RunByLeg;
+}
+
+class Dog implements Runnable {
+  use RunByLeg;
+}
+
+class Car implements Runnable {
+  use RunByWheel;
+}
+
+class Knight implements Runnable{
+  use RunByHorse;
+}
+
+$human = new Human();
+if ($human instanceof Runnable){
+  echo "人間は走れます\n";
+  $human->run();
+}
+
+$dog = new Dog();
+if($dog instanceof Dog){
+  echo "犬は走れます\n";
+  $dog->run();
+}
+
+
+$car = new Car();
+if($car instanceof Car){
+  echo "車は走れます\n";
+  $car->run();
+}
+
+$knight = new Knight();
+if($knight instanceof Knight){
+  echo "騎士は走れます\n";
+  $knight->run();
+}
