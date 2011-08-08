8b92f8a4cc7c2ed8d8d4e189b0c6dfa38e2fab52
kamekoopa <hogehugo.gmail.com>
2011-08-08 10:17:39 +0900

    新配列定義テスト

---
+++ array_short_definition.php
@@ -0,0 +1,12 @@
+<?php
+
+  $array1 = [1,2,3,4,5,];
+
+  $array2 = ["key1" => "value1", "key2" => "value2",];
+
+  $array3 = [1,2,3,4,"key3" => "value3",];
+
+
+  print_r($array1);
+  print_r($array2);
+  print_r($array3);
